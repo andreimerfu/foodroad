300f53084543ef4de12396ddd8100768784618b5
Antonela Perescu <antonela-madalina.perescu@my.fmi.unibuc.ro>
2018-11-07 10:58:19 +0200

    Fix layout

--- client/src/components/home/RestaurantSearch.js
+++ client/src/components/home/RestaurantSearch.js
@@ -7,10 +7,10 @@ export class RestaurantSearch extends React.Component {
 		return(
 			<div className='home-container'>
 				<div className="row row-search">
-			        <div className="col-md-6 search-input">
+			        <div className="col-lg-5 search-input">
 		                <input type="text" className="form-control input-lg" placeholder="Search location" />
 			        </div>
-			        <div className="col-md-6">
+			        <div className="col-lg-4">
 		                <button className="btn btn-info search-btn" type="button">
 							<i className="fas fa-search-location"></i>
 		                </button>
