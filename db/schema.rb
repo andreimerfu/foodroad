0e2a113858a029868cc5712d4cdb2b263ef60e86
Andrei Merfu <andrei.merfu@my.fmi.unibuc.ro>
2018-11-11 16:24:01 +0200

    Add restaurant image url

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_10_135105) do
+ActiveRecord::Schema.define(version: 2018_11_11_142017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_11_10_135105) do
     t.float "lng", default: 0.0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
