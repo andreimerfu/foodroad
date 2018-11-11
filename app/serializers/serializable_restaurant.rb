389ecb7f264c7bd2ed3f47758ac6b4d73d8accca
Andrei Merfu <andrei.merfu@my.fmi.unibuc.ro>
2018-11-11 16:28:45 +0200

    Add more attributes in restaurant serializer

--- app/serializers/serializable_restaurant.rb
+++ app/serializers/serializable_restaurant.rb
@@ -3,7 +3,7 @@
 class SerializableRestaurant < JSONAPI::Serializable::Resource
   type 'restaurants'
 
-  attributes :name, :address
+  attributes :name, :address, :delivery_zone, :image, :min_order, :delivery_time
 
   link :self do
     @url_helpers.api_v1_restaurant_path(@object.id)
