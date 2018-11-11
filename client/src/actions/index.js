8bfce4efd76804fb0ce8808632a676603fd14593
Andrei Merfu <andrei.merfu@my.fmi.unibuc.ro>
2018-11-11 16:54:20 +0200

    Fix redirect email url

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -38,7 +38,7 @@ const registerFailure = (errors) => {
 }
 
 export const register = (userData) => {
-  var confirm_success_url = "http://localhost:3001/login";
+  var confirm_success_url = window.location.href + "login";
   return dispatch => {
     return axios.post('/auth', {...userData, confirm_success_url})
       .then(res => {
