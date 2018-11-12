39f155fa78c35ab9d3c8fcd735ae9efc59ab2eb2
Andrei Merfu <andrei.merfu@sparktech.ro>
2018-11-12 14:31:20 +0200

    Add category serializable

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,30 +1,30 @@
 # frozen_string_literal: true
 
 class Api::V1::CategoriesController < ApplicationController
-  before_action -> { is_authenticated_as(:admin) }, only: [:create, :update, :destroy]
+  before_action -> { is_authenticated_as(:restaurant) }, only: [:create, :update, :destroy]
 
   def index
     categories = Category.all
-    render json: categories, status: :ok
+    render jsonapi: categories, status: :ok
   end
 
   def create
     category = Category.new(categories_params)
     if category.save
-      render json: category, status: :created
+      render jsonapi: category, status: :created
     else
-      render json: category.errors, status: :unprocessable_entity
+      render jsonapi_errors: category.errors, status: :unprocessable_entity
     end
   end
 
   def update
     category = Category.update(categories_params)
-    render json: category, status: :ok
+    render jsonapi: category, status: :ok
   end
 
   def destroy
     category = Category.destroy(params[:id])
-    render json: category, status: :ok
+    render jsonapi: category, status: :ok
   end
 
   private
