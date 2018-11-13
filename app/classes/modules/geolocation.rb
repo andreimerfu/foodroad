3713d5211ede13a3950292a3153847a8dcbb8b82
Andrei Merfu <andrei.merfu@sparktech.ro>
2018-11-13 09:37:03 +0200

    Refactor restaurant model

---
+++ app/classes/modules/geolocation.rb
@@ -0,0 +1,18 @@
+module Modules
+  module Geolocation
+    def nearest_restaurants(location, restaurants)
+      nearest_restaurants = []
+      restaurants.each do |restaurant|
+        destination = [restaurant.lat, restaurant.lng]
+        if distance_between_2_points(location, destination) <= restaurant.delivery_zone
+          nearest_restaurants << restaurant
+        end
+      end
+      nearest_restaurants
+    end
+
+    def self.included(receiver)
+      receiver.extend Geolocation
+    end
+  end
+end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,24 +3,11 @@
 class Restaurant < ApplicationRecord
   has_many :products
   has_many :categories, through: :products
-
+  include Modules::Geolocation
 
   def self.find_nearest_restaurants(lat, lng)
     restaurants = Restaurant.all
     current_location = [lat.to_f, lng.to_f]
     nearest_restaurants(current_location, restaurants)
   end
-
-  private
-
-    def self.nearest_restaurants(location, restaurants)
-      nearest_restaurants = []
-      restaurants.each do |restaurant|
-        destination = [restaurant.lat, restaurant.lng]
-        if distance_between_2_points(location, destination) <= restaurant.delivery_zone
-          nearest_restaurants << restaurant
-        end
-      end
-      nearest_restaurants
-    end
 end
