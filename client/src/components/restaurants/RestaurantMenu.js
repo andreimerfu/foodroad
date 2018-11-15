697a5c3695221b3297065441acb77b6d2ef80283
Antonela Madalina Perescu <am.perescu@gmail.com>
2018-11-15 22:46:43 +0200

    Fix restaurant menu component bug

--- client/src/components/restaurants/RestaurantMenu.js
+++ client/src/components/restaurants/RestaurantMenu.js
@@ -39,17 +39,24 @@ class RestaurantMenu extends React.Component {
     })
   }
 
+  renderRestaurantName(restaurant) {
+    return(
+        <h3>{restaurant.attributes.name}</h3>
+      )
+  }
+
   render(){
     const categories = this.props.categories;
     const restaurant = this.props.restaurant;
     const products = this.props.products;
-    if (categories.length > 0) {
+
+    if (categories.length > 0 && Object.keys(restaurant).length === 5) {
       return(
         <section id='restaurant-menu'>
         <div className="wrapper">
           <nav id="sidebar">
               <div className="sidebar-header">
-                  <h3>{restaurant.attributes.name}</h3>
+                  {this.renderRestaurantName(restaurant)}
               </div>
 
               <ul className="list-unstyled components">
@@ -58,70 +65,16 @@ class RestaurantMenu extends React.Component {
 
               <ul className="list-unstyled CTAs">
                   <li>
-                      <a href="#" className="download">Recenzii</a>
+                      <a href="" className="download">Recenzii</a>
                   </li>
                   <li>
-                      <a href="#" className="article">Info</a>
+                      <a href="" className="article">Info</a>
                   </li>
               </ul>
           </nav>
 
-
           <div id="content">
-             {/* <nav className="nav-toggle navbar-expand-lg navbar-light bg-light">
-                  <div className="container-fluid">
-
-                      <button type="button" id="sidebarCollapse" className="btn btn-info">
-                          <i className="fas fa-align-left"></i>
-                          <span>Toggle Sidebar</span>
-                      </button>
-
-                      <button className="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                          <i className="fas fa-align-justify"></i>
-                      </button>
-
-                      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-
-                          <ul className="nav navbar-nav ml-auto">
-                              <li className="nav-item active">
-                                  <a className="nav-link" href="#">Page</a>
-                              </li>
-                              <li className="nav-item">
-                                  <a className="nav-link" href="#">Page</a>
-                              </li>
-                              <li className="nav-item">
-                                  <a className="nav-link" href="#">Page</a>
-                              </li>
-                              <li className="nav-item">
-                                  <a className="nav-link" href="#">Page</a>
-                              </li>
-                          </ul>
-
-                      </div>
-
-                  </div>
-              </nav>*/}
-
-              {this.renderCategoriesWithProducts()}
-
-              {/*<h2>Collapsible Sidebar Using Bootstrap 4</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-              <div className="line"></div>
-
-              <h2>Lorem Ipsum Dolor</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-              <div className="line"></div>
-
-              <h2>Lorem Ipsum Dolor</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-              <div className="line"></div>
-
-              <h3>Lorem Ipsum Dolor</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>*/}
+            {this.renderCategoriesWithProducts()}
           </div>
       </div>
       </section>
