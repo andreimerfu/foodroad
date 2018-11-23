1a745891513a7be64e58b9cbde71458a614f3ec3
Marius Micluta-Campeanu <marius.micluta-campeanu@my.fmi.unibuc.ro>
2018-11-24 01:26:49 +0200

    Misc refactor

--- spec/requests/profiles_requests_spec.rb
+++ spec/requests/profiles_requests_spec.rb
@@ -8,30 +8,30 @@ describe 'Profiles' do
   include_context 'shared users'
   include_context 'shared profiles'
 
-  context 'when things work' do
+  context 'authorized' do
     it '#show' do
       get api_v1_profiles_path, headers: headers
-      expect(response.status).to eq(200)
+      expect(response).to have_http_status(:ok)
     end
 
     it '#update' do
       expect(user.profile.addresses).to be_empty
       put api_v1_profiles_path, headers: headers, params: update_params, as: :json
       user.reload
-      expect(response.status).to eq(200)
+      expect(response).to have_http_status(:ok)
       expect(user.profile.addresses.length).to eq(2)
     end
   end
 
-  context 'when not authorized' do
+  context 'not authorized' do
     it '#show' do
       get api_v1_profiles_path
-      expect(response.status).to eq(401)
+      expect(response).to have_http_status(:unauthorized)
     end
 
     it '#update' do
       put api_v1_profiles_path, params: update_params
-      expect(response.status).to eq(401)
+      expect(response).to have_http_status(:unauthorized)
     end
   end
 end
