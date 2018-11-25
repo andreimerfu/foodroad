0ad5b3286c64af5b5208458021f13abfb3b828c4
Andrei Merfu <andrei.merfu@my.fmi.unibuc.ro>
2018-11-25 11:41:29 +0200

    Integrate active_storage attachment on Restaurant Model

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resource :profiles, only: [:show, :update, :destroy]
-      resources :restaurants, only: [:index, :show, :create] do
+      resources :restaurants do
         resources :products, module: :restaurants
         resources :categories, only: [:index, :create, :update, :destroy], module: :restaurants
       end
