d1707f6cb5e58c824c61d283128b35e2b9ee524e
Marius Micluta-Campeanu <marius.micluta-campeanu@my.fmi.unibuc.ro>
2018-11-25 13:54:02 +0200

    Add backend for admin page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :categories, only: %i(index create update destroy)
+    resources :products
+    resources :restaurants, only: %i(index show create)
+
+    root to: 'restaurants#index'
+  end
+
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
