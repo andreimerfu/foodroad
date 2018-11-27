49d224f2b4046e8c953c497b500957c6326a8d49
Marius Micluta-Campeanu <marius.micluta-campeanu@gitlab.com>
2018-11-28 00:42:34 +0200

    Improve update addresses functionality

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -6,7 +6,7 @@ class Profile < ApplicationRecord
   validate :valid_addresses
 
   def valid_addresses
-    unless addresses.map(&:keys).all? { |keys| keys == %w(address tag) }
+    unless addresses.map(&:keys).all? { |keys| keys.all? { |key| key.in? %w(address tag _destroy) } }
       errors.add(:addresses, 'Invalid format')
     end
   end
