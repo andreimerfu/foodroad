19ae7106190fcd1e5db50438b57a99d51559d7f0
Antonela Madalina Perescu <am.perescu@gmail.com>
2018-12-18 16:53:13 +0200

    Fix

--- client/src/components/login/LoginForm.js
+++ client/src/components/login/LoginForm.js
@@ -3,12 +3,23 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { AuthInput } from '../shared/form/AuthInput';
 import { ResErrors } from '../shared/form/ResErrors';
+import { required, minLength4 } from '../shared/form/validators';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+function notify(errors) { 
+  console.log(errors);
+  toast.error(errors)
+}
+
 
 const LoginForm = props => {
-  const { handleSubmit, pristine, submitting, submitCb, errors} = props
+  const { handleSubmit, pristine, submitting, submitCb, valid, errors} = props
+
   return (
    <div>
-    <ResErrors errors={errors} />
+   <ResErrors errors={errors} />
     <form onSubmit={handleSubmit(submitCb)}>
       <Field
         name="email"
@@ -16,6 +27,7 @@ const LoginForm = props => {
         label='Email address'
         className='form-control border-0 shadow'
         component={AuthInput}
+        validate={[required]}
       />
       <Field
         name="password"
@@ -23,11 +35,13 @@ const LoginForm = props => {
         label='Password'
         className='form-control border-0 shadow text-violet'
         component={AuthInput}
+        validate={[required, minLength8]}
       />
       <div>
-        <button className='btn btn-primary shadow px-5 my-4' type="submit" disabled={ pristine || submitting }>
+        <button className='btn btn-primary shadow px-5 my-4' onClick={notify(errors)} type="submit" disabled={!valid || pristine || submitting }>
           Log In
         </button>
+        <ToastContainer />
       </div>
     </form>
     </div>
